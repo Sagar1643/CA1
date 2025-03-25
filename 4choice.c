68c006a6dd237b022dac47382aed821e0dcf2ec7
Sagar1643 <sagar.r@s.amity.edu>
2025-03-25 11:43:34 +0530

    Choice1 Btech CSE

--- 4choice.c
+++ 4choice.c
@@ -15,15 +15,7 @@ int main() {
         case 1:
             printf("You selected BTech CSE.\n");
             break;
-        case 2:
-            printf("You selected BTech AIML.\n");
-            break;
-        case 3:
-            printf("You selected BTech ECE.\n");
-            break;
-        case 4:
-            printf("You selected BTech Mechanical.\n");
-            break;
+      
         default:
             printf("Invalid choice. Please select a valid option (1-4).\n");
     }
